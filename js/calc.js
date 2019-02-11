9f3de1526cac379ad5f21c27e4c79c20d5ef18f3
Davy de Matos <davymatos01@gmail.com>
2019-02-11 16:43:14 -0300

    v1.0

---
+++ js/calc.js
@@ -0,0 +1,14 @@
+function botao(num){
+	var concat = document.calc.tela.value;
+	document.calc.tela.value = concat + num;	
+}
+function resultado(){
+	resultado = 0;
+	resultado = document.getElementById("tela").value;
+
+	document.getElementById("tela").value = '';
+	document.getElementById("tela").value = eval(resultado);
+}
+function limpar(){
+	document.getElementById("tela").value = '';
+}
